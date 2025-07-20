7f0b25cf452946975d671aae43521a297c12ca1f
A.N.C <alrizenexcell@gmail.com>
2025-07-20 11:16:28 +0530

    Add files via upload
    this the 3 rd try for auto upload, done NEll & Subhayu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import img from './assets/logo_black.png'
+import { FaInstagram } from "react-icons/fa6";
 
 const App = () => {
   return <div className="container">
@@ -7,12 +8,12 @@ const App = () => {
       <img src={img} style={{ width: "100px" }} />
       <p style={{ fontSize: "24px", textDecoration: "underline" }}>Alrize Nex Cell</p>
     </div>
-    <div style={{ textAlign: "center", paddingBottom: "100px" }}>
+    <div style={{ textAlign: "center", paddingBottom: "120px" }}>
       <p style={{ marginBottom: "12px", fontSize: "21px" }}>Introducing</p>
       <p style={{ maxWidth: "450px", fontSize: "12px" }}>We deliver scalable web platforms with seamless UI/UX, while crafting strategic content and creative assets—combining digital precision with compelling brand storytelling to elevate experiences across design, development, and marketing.</p>
     </div>
     <div style={{ height: "100px" }}>
-      <a href='https://www.instagram.com/anc.alrize?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D' className='button'>Our Instagram</a>
+      <a href='https://www.instagram.com/anc.alrize?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw%3D%3D' className='button'><FaInstagram /> Our Instagram</a>
     </div>
   </div>;
 };
